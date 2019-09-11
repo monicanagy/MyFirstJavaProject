a3dc8fa5d94c3f814442f10bc17ab8c3c2aea1e3
Monica <monica.nagy1986@gmail.com>
2019-09-11 20:49:25 +0300

    jhj

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        //Tipuri de date
+        String brand = "Zara";
+        String colour = "Verde";
+        byte size = 34;
+        int beautyLevel = 123484848;
+        long utilityLevel = 111111111147372737L;
+        double satisfactionLevel = 11111111111112636747577552d;
+
+
+    }
+}
