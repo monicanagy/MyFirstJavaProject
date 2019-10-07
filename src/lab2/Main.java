b0e079028ea5ceb4ef69c875da7d18f541f1dfcf
Monica <monica.nagy1986@gmail.com>
2019-10-07 21:42:32 +0300

    lab1

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -2,18 +2,19 @@ package lab2;
 
 public class Main {
 
-    public static int memberVariable= 3;
-
     public static void main(String[] args) {
-
 
 
         int x = 3;
         int y = 5;
-        float result = sum(x,y);
+        float result = sum(x, y);
         System.out.println(result);
-        result= div(x,y);
-        result= min(x,y);
+        result=min(x,y);
+        System.out.println(result);
+        result=div(x,y);
+        System.out.println(result);
+        result=mult(x,y);
+        System.out.println(result);
 
 
 
@@ -21,25 +22,32 @@ public class Main {
 
     }
 
-    public static int sum(int first, int second ) {
-        int sum = first+ second;
+
+
+
+
+    public static int sum(int first, int second) {
+        int sum = first + second;
         return sum;
     }
-
+    public static float min(float first, float second) {
+        float min = first - second;
+        return min;
+    }
     public static float div(float first, float second){
-        float div = first/second;
+        float div= first/second;
         return div;
     }
-    public static float min(float first, float second){
-        float min = first-second;
-        return min;
-
+    public static int mult(int first, int second){
+        int mult= first*second;
+        return mult;
     }
 
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
